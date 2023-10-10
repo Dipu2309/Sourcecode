72dee63336b0acd5b5e88d9ab96a32567c095c02
Dipu2309 <dileep.behera@yahoo.com>
2023-10-10 15:43:44 +0530

    Ways to create String

---
+++ Java/src/october/String/StringCreation.java
@@ -0,0 +1,14 @@
+package october.String;
+
+public class StringCreation {
+
+	public static void main(String[] args) {
+		// String Literal
+		String str1="Dileep";
+		//using new keyword
+		String str2=new String("Kumar");
+		
+		System.out.println("Using Literal:"+str1+" and Using New keyword:"+str2);
+	}
+
+}

27f668eb71041423337680da91bd252eab9ae99e
Dipu2309 <dileep.behera@yahoo.com>
2023-10-25 00:44:15 +0530

    Library fine

---
+++ HackerRank/src/com/easy/Library_Fine.java
@@ -0,0 +1,42 @@
+package com.easy;
+
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class Library_Fine {
+
+    public static void main(String x[]) {
+        Library_Fine o = new Library_Fine();
+        o.run();
+    }
+
+    void run() {
+        try (final Scanner in = new Scanner(System.in, "ascii")) {
+
+            final int fine;
+
+            final int aday = in.nextInt();
+            final int amon = in.nextInt();
+            final int ayr = in.nextInt();
+
+            final int eday = in.nextInt();
+            final int emon = in.nextInt();
+            final int eyr = in.nextInt();
+
+            if (ayr < eyr) {
+                fine = 0;
+            } else if (ayr > eyr) {
+                fine = 10_000;
+            } else if (amon < emon) {
+                fine = 0;
+            } else if (amon > emon) {
+                fine = 500 * (amon - emon);
+            } else if (aday > eday) {
+                fine = 15 * (aday - eday);
+            } else {
+                fine = 0;
+            }
+            out.println(fine);
+        }
+    }
+}
